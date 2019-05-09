48effe04d22f8497d260b48db58029c3f393fdbe
Olakunle Salami <kunsal2003@gmail.com>
2019-05-10 00:30:59 +0100

    refactored sequelize initialization

--- models/index.js
+++ models/index.js
@@ -1,38 +1,32 @@
-'use strict';
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+      , sequelize = null
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+  if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    const env = process.env.NODE_ENV || 'development';
+    const config = require(__dirname + '/../config/config.json')[env];
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+  }
+
+  /*
+    Associations can be defined here. E.g. like this:
+    global.db.User.hasMany(global.db.SomethingElse)
+  */
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
-//global.sequelize = sequelize;
+module.exports = global.db
